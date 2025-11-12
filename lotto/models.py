cdc66971fe941d23a5e8bc80c0a37f8c109164bc
Rachel1028 <heoyunseo72@gmail.com>
2025-11-12 16:03:33 +0900

    feat:basic lotto app 구현 완료

--- lotto/models.py
+++ lotto/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+import random
 
-# Create your models here.
+class LottoTicket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    numbers = models.CharField(max_length=30)
+    is_winner = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.numbers}'
+
+    @staticmethod
+    def generate_auto_numbers():
+        nums = sorted(random.sample(range(1, 46), 6))
+        return ', '.join(map(str, nums))
--- lotto/views.py
+++ lotto/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import LottoTicket
+from django.contrib.auth.models import User
 
-# Create your views here.
+def home(request):
+    tickets = LottoTicket.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'tickets': tickets})
+
+def buy_ticket(request):
+    user, _ = User.objects.get_or_create(username='guest')  # 임시 사용자
+    if request.method == 'POST':
+        mode = request.POST.get('mode')
+        if mode == 'auto':
+            numbers = LottoTicket.generate_auto_numbers()
+        else:
+            numbers = request.POST.get('numbers')
+        LottoTicket.objects.create(user=user, numbers=numbers)
+        return redirect('home')
+    return render(request, 'buy.html')
+
